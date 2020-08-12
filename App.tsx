509ec388b7fca2fd6a7d7de689bd6275e8aac036
charley mcgowan <charley.mcgowan@outlook.com>
2020-08-12 16:11:30 -0700

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,6 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { Text } from './components/Themed';
+
 
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
